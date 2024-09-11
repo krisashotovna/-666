2e842d7dc8d45ce327470d8a3bb019b12a290c9f
krisashotovna <161230179+krisashotovna@users.noreply.github.com>
2024-09-11 23:59:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 0;
+            bool isCalculating = true;
+
+            while (isCalculating)
+            {
+                Console.Write("Введите первое число: ");
+                double num1 = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите операцию (+, -, *, /): ");
+                char operation = char.Parse(Console.ReadLine());
+
+                Console.Write("Введите второе число: ");
+                double num2 = double.Parse(Console.ReadLine());
+
+                switch (operation)
+                {
+                    case '+':
+                        result = num1 + num2;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+                    case '/':
+                        if (num2 == 0)
+                        {
+                            Console.WriteLine("Ошибка: деление на ноль");
+                        }
+                        else
+                        {
+                            result = num1 / num2;
+                            Console.WriteLine("Результат: " + result);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка: неверная операция");
+                        break;
+                }
+
+                Console.Write("Хотите продолжить вычисления? (да/нет): ");
+                string continueInput = Console.ReadLine();
+
+                if (continueInput.ToLower() == "нет")
+                {
+                    isCalculating = false;
+                    Console.WriteLine("Программа завершена.");
+                }
+                else
+                {
+                    Console.WriteLine("Продолжаем вычисления...");
+                }
+            }
+        }
+    }
+}
